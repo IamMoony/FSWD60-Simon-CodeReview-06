360bd2cf677a583547ae0ed4a7e5981328d0ede4
Simon Blaha <s.blaha@live.at>
2019-02-15 23:58:18 +0100

    add data and new images, create for loop to loop through data and display it into the html

--- js/script.js
+++ js/script.js
@@ -12,7 +12,8 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var locations = /** @class */ (function () {
-    function locations(city, zipcode, address, image) {
+    function locations(name, city, zipcode, address, image) {
+        this.name = name;
         this.city = city;
         this.zipcode = zipcode;
         this.address = address;
@@ -25,8 +26,8 @@ var locations = /** @class */ (function () {
 }());
 var places = /** @class */ (function (_super) {
     __extends(places, _super);
-    function places(city, zipcode, address, image) {
-        return _super.call(this, city, zipcode, address, image) || this;
+    function places(name, city, zipcode, address, image) {
+        return _super.call(this, name, city, zipcode, address, image) || this;
     }
     places.prototype.render = function () {
         return "\n\t\t" + _super.prototype.render.call(this) + "\n\t\t<div class=\"row\">\n\t\t<div class=\"col-lg-6 images\">\n\t\t<img src=\"" + this.image + "\">\n\t\t</div>\n\t\t<div class=\"col-lg-6 desciption\">\n\t\t<p>City: " + this.city + "</p>\n\t\t<p>ZIP-code: " + this.zipcode + "</p>\n\t\t<p>Address: " + this.address + "</p>";
@@ -35,22 +36,22 @@ var places = /** @class */ (function (_super) {
 }(locations));
 var restaurant = /** @class */ (function (_super) {
     __extends(restaurant, _super);
-    function restaurant(city, zipcode, address, image, tel, type, website) {
-        var _this = _super.call(this, city, zipcode, address, image, tel, type, website) || this;
+    function restaurant(name, city, zipcode, address, image, tel, type, website) {
+        var _this = _super.call(this, name, city, zipcode, address, image, tel, type, website) || this;
         _this.tel = tel;
         _this.type = type;
         _this.website = website;
         return _this;
     }
     restaurant.prototype.render = function () {
-        return "\n\t\t" + _super.prototype.render.call(this) + "\n\t\t<div class=\"row\">\n\t\t<div class=\"col-lg-6 images\">\n\t\t<img src=\"" + this.image + "\">\n\t\t</div>\n\t\t<div class=\"col-lg-6 desciption\">\n\t\t<p>City: " + this.city + "</p>\n\t\t<p>ZIP-code: " + this.zipcode + "</p>\n\t\t<p>Address: " + this.address + "</p>\n\t\t<p>Tel: " + this.tel + "</p>\n\t\t<p>Type: " + this.type + "</p>\n\t\t<p>Url:  " + this.website + "</p>\n\t\t</div>\n\t\t</div>\n\t\t";
+        return "\n\t\t" + _super.prototype.render.call(this) + "\n\t\t<div class=\"row\">\n\t\t<div class=\"col-lg-6 images\">\n\t\t<h1>" + this.name + "</h1>\n\t\t<img src=\"" + this.image + "\">\n\t\t</div>\n\t\t<div class=\"col-lg-6 desciption\">\n\t\t<p>City: " + this.city + "</p>\n\t\t<p>ZIP-code: " + this.zipcode + "</p>\n\t\t<p>Address: " + this.address + "</p>\n\t\t<p>Tel: " + this.tel + "</p>\n\t\t<p>Type: " + this.type + "</p>\n\t\t<p>Url:  " + this.website + "</p>\n\t\t</div>\n\t\t</div>\n\t\t";
     };
     return restaurant;
 }(locations));
 var events = /** @class */ (function (_super) {
     __extends(events, _super);
-    function events(city, zipcode, address, image, eventdate, ticketprice) {
-        var _this = _super.call(this, city, zipcode, address, image, eventdate, ticketprice) || this;
+    function events(name, city, zipcode, address, image, eventdate, ticketprice) {
+        var _this = _super.call(this, name, city, zipcode, address, image, eventdate, ticketprice) || this;
         _this.eventdate = eventdate;
         _this.ticketprice = ticketprice;
         return _this;
@@ -60,12 +61,38 @@ var events = /** @class */ (function (_super) {
     };
     return events;
 }(locations));
+//test start
 // (city, zipcode, address, image)
-var place1 = new places("Vienna", 1440, "Rathausplatz", "img/charles.jpg");
-document.getElementById("places").innerHTML += place1.render();
-var restaurant1 = new restaurant("Vienna", 1220, "Franzgasse", "img/charles.jpg", "0660007", "Chinese", "www.food.com");
-document.getElementById("places").innerHTML += restaurant1.render();
-var restaurant2 = new restaurant("Korneuburg", 1220, "Rathausplatz", "img/sixta.png", "08002222", "Austrian", "www.food.kbg");
-document.getElementById("places").innerHTML += restaurant2.render();
-var event1 = new events("Vienna", 1220, "Franzgasse", "img/kris.jpg", "10.11.2019", 200);
-document.getElementById("places").innerHTML += event1.render();
+// var place1 = new places("Eiffel Tower", "Vienna, Austria", 1440, "Rathausplatz", "img/charles.jpg")
+// document.getElementById("places").innerHTML += place1.render();
+// var restaurant1 = new restaurant("Vienna", 1220, "Franzgasse", "img/charles.jpg", "0660007", "Chinese", "www.food.com")
+// document.getElementById("places").innerHTML += restaurant1.render();
+// var restaurant2 = new restaurant("Korneuburg", 1220, "Rathausplatz", "img/sixta.png", "08002222", "Austrian", "www.food.kbg")
+// document.getElementById("places").innerHTML += restaurant2.render();
+// var event1 = new events("Vienna", 1220, "Franzgasse", "img/kris.jpg", "10.11.2019", 200);
+// document.getElementById("places").innerHTML += event1.render();
+// test end
+//Data start
+var placesData = new Array();
+// name, city, zipcode, address, image
+placesData[0] = new places("Eiffel Tower", "Paris", 75007, "5 avenue Anatole France", "img/eiffel.jpg");
+placesData[1] = new places("St. Stephens Cathedral", "Vienna, Austria", 1010, "Stephansplatz 3, 1010 Wien", "img/stephansdom.jpg");
+placesData[2] = new places("Chichen Itza", "Yucatán, Mexico", "unknown", "unknown", "img/mayatemple.jpg");
+placesData[3] = new places("Broadway Theatre", "New York City", 10036, "Theater District, Manhattan", "img/broadway.jpg");
+var restaurantData = new Array();
+// name, city, zipcode, address, image, tel, type, website
+restaurantData[0] = new restaurant("DO & CO Restaurant Stephansplatz", "Vienna, Austria", 1010, "Stephansplatz 12", "img/doco.jpg", "01 5353969", "Luxury", "https://www.docohotel.com/de/restaurant");
+restaurantData[0] = new restaurant("TGI Fridays", "Vienna, Austria", 1010, "Schubertring 13", "img/tgi.jpg", " 01 7148995", "Fast Food", "http://tgifridays.at/");
+restaurantData[0] = new restaurant("DO & CO Restaurant Stephansplatz", "Vienna, Austria", 1010, "Stephansplatz 12", "img/doco.jpg", "015353969", "Allround", "https://www.docohotel.com/de/restaurant");
+restaurantData[0] = new restaurant("DO & CO Restaurant Stephansplatz", "Vienna, Austria", 1010, "Stephansplatz 12", "img/doco.jpg", "015353969", "Allround", "https://www.docohotel.com/de/restaurant");
+var eventData = new Array();
+// (name, city, zipcode, address, image, eventdate, ticketprice)
+eventData[0] = new events("");
+function drawContentPlaces() {
+    for (var index in placesData) {
+        document.getElementById("places").innerHTML += placesData[index].render();
+    }
+}
+function drawContentRestaurants() {
+}
+drawContentPlaces();
--- js/script.ts
+++ js/script.ts
@@ -1,11 +1,12 @@
 class locations{
-
+	name : string;
 	city : string;
-	zipcode : number;
+	zipcode : any;
 	address : string;
 	image : any;
 
-	constructor(city, zipcode, address, image){
+	constructor(name, city, zipcode, address, image){
+		this.name = name;
 		this.city = city;
 		this.zipcode = zipcode;
 		this.address = address;
@@ -20,8 +21,8 @@ class locations{
 
 class places extends locations {
 
-	constructor(city, zipcode, address, image){
-		super(city, zipcode, address, image);
+	constructor(name, city, zipcode, address, image){
+		super(name, city, zipcode, address, image);
 	}
 	render(){
 		return `
@@ -42,8 +43,8 @@ class restaurant extends locations{
 	type : string;
 	website : any;
 
-	constructor(city, zipcode, address, image, tel, type, website){
-		super(city, zipcode, address, image, tel, type, website);
+	constructor(name, city, zipcode, address, image, tel, type, website){
+		super(name, city, zipcode, address, image, tel, type, website);
 		this.tel = tel;
 		this.type = type;
 		this.website = website;
@@ -54,6 +55,7 @@ class restaurant extends locations{
 		${super.render()}
 		<div class="row">
 		<div class="col-lg-6 images">
+		<h1>${this.name}</h1>
 		<img src="${this.image}">
 		</div>
 		<div class="col-lg-6 desciption">
@@ -74,8 +76,8 @@ class events extends locations{
 	eventdate : string;
 	ticketprice : number;
 
-	constructor(city, zipcode, address, image, eventdate, ticketprice){
-		super(city, zipcode, address, image, eventdate, ticketprice);
+	constructor(name, city, zipcode, address, image, eventdate, ticketprice){
+		super(name, city, zipcode, address, image, eventdate, ticketprice);
 		this.eventdate = eventdate;
 		this.ticketprice = ticketprice;
 }
@@ -99,16 +101,55 @@ render(){
 	}
 
 }
+
+//test start
 // (city, zipcode, address, image)
-var place1 = new places("Vienna", 1440, "Rathausplatz", "img/charles.jpg")
-document.getElementById("places").innerHTML += place1.render();
+// var place1 = new places("Eiffel Tower", "Vienna, Austria", 1440, "Rathausplatz", "img/charles.jpg")
+// document.getElementById("places").innerHTML += place1.render();
 
-var restaurant1 = new restaurant("Vienna", 1220, "Franzgasse", "img/charles.jpg", "0660007", "Chinese", "www.food.com")
-document.getElementById("places").innerHTML += restaurant1.render();
+// var restaurant1 = new restaurant("Vienna", 1220, "Franzgasse", "img/charles.jpg", "0660007", "Chinese", "www.food.com")
+// document.getElementById("places").innerHTML += restaurant1.render();
 
-var restaurant2 = new restaurant("Korneuburg", 1220, "Rathausplatz", "img/sixta.png", "08002222", "Austrian", "www.food.kbg")
-document.getElementById("places").innerHTML += restaurant2.render();
+// var restaurant2 = new restaurant("Korneuburg", 1220, "Rathausplatz", "img/sixta.png", "08002222", "Austrian", "www.food.kbg")
+// document.getElementById("places").innerHTML += restaurant2.render();
 
-var event1 = new events("Vienna", 1220, "Franzgasse", "img/kris.jpg", "10.11.2019", 200);
-document.getElementById("places").innerHTML += event1.render();
+// var event1 = new events("Vienna", 1220, "Franzgasse", "img/kris.jpg", "10.11.2019", 200);
+// document.getElementById("places").innerHTML += event1.render();
+// test end
+
+//Data start
+var placesData = new Array();
+// name, city, zipcode, address, image
+placesData[0] = new places("Eiffel Tower", "Paris",  75007, "5 avenue Anatole France", "img/eiffel.jpg")
+placesData[1] = new places("St. Stephens Cathedral", "Vienna, Austria",  1010, "Stephansplatz 3, 1010 Wien", "img/stephansdom.jpg")
+placesData[2] = new places("Chichen Itza", "Yucatán, Mexico",  "unknown", "unknown", "img/mayatemple.jpg")
+placesData[3] = new places("Broadway Theatre", "New York City",  10036, "Theater District, Manhattan", "img/broadway.jpg")
+
+var restaurantData = new Array();
+// name, city, zipcode, address, image, tel, type, website
+restaurantData[0] = new restaurant("DO & CO Restaurant Stephansplatz", "Vienna, Austria", 1010, "Stephansplatz 12", "img/doco.jpg", "01 5353969", "Luxury", "https://www.docohotel.com/de/restaurant")
+restaurantData[0] = new restaurant("TGI Fridays", "Vienna, Austria", 1010, "Schubertring 13", "img/tgi.jpg", " 01 7148995", "Fast Food", "http://tgifridays.at/")
+restaurantData[0] = new restaurant("DO & CO Restaurant Stephansplatz", "Vienna, Austria", 1010, "Stephansplatz 12", "img/doco.jpg", "015353969", "Allround", "https://www.docohotel.com/de/restaurant")
+restaurantData[0] = new restaurant("DO & CO Restaurant Stephansplatz", "Vienna, Austria", 1010, "Stephansplatz 12", "img/doco.jpg", "015353969", "Allround", "https://www.docohotel.com/de/restaurant")
+
+var eventData = new Array();
+
+// (name, city, zipcode, address, image, eventdate, ticketprice)
+eventData[0] = new events("")
+
+
+function drawContentPlaces() {
+	for (let index in placesData) {
+	document.getElementById("places").innerHTML += placesData[index].render();
+	}
+}
+
+function drawContentRestaurants () {
+
+}
+
+drawContentPlaces();
+
+
+
 
